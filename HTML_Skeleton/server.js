23ea9c5ccb17c49b3240104a94a38351e9a1b6f7
mooz4305 <mooz4305@colorado.edu>
2019-11-10 21:15:13 -0700

    Introduced basic server.js file so web page can be served statically. Modules for NodeJS added. Removed references in .html files to nonexistent stylesheets.

---
+++ HTML_Skeleton/server.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+// statically serve the files (images, javascript, css...) in current directory.
+// NOTE: may need to serve files in 'resources' instead, for better security; this requires fixing urls in html files.
+app.use(express.static(__dirname));
+
+var view_dir = path.join(__dirname + "/views")
+
+// send html files to browser, depending on url request.
+app.get('/homepage.html', function(req, res){
+    res.sendFile('homepage.html', { root: view_dir } );
+});
+
+app.get('/login.html', function(req, res){
+    res.sendFile('login.html', { root: view_dir } );
+});
+
+app.get('/resourcepage.html', function(req, res){
+    res.sendFile('resourcepage.html', { root: view_dir } );
+});
+
+app.get('/staff_form.html', function(req, res){
+    res.sendFile('staff_form.html', { root: view_dir } );
+});
+
+app.get('/staff-page.html', function(req, res){
+    res.sendFile('staff-page.html', { root: view_dir } );
+});
+
+app.get('/survey.html', function(req, res){
+    res.sendFile('survey.html', { root: view_dir } );
+});
+
+// create server at port 3000
+app.listen(3000, function () {
+    console.log('Listening on http://localhost:3000/');
+});
