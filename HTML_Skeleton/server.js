5d57c508dc912d3a28d57cf1fcee0c86f020e208
mooz4305 <mooz4305@colorado.edu>
2019-11-12 09:52:38 -0700

    Updated server.js with javascript function that can emulate form requests.

--- HTML_Skeleton/server.js
+++ HTML_Skeleton/server.js
@@ -2,6 +2,40 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+
+// Code from https://stackoverflow.com/a/133997
+/**
+ * sends a request to the specified url from a form. this will change the window location.
+ * @param {string} path the path to send the post request to
+ * @param {object} params the paramiters to add to the url
+ * @param {string} [method=post] the method to use on the form
+ */
+
+function post(path, params, method='post') {
+
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
+
+
 // statically serve the files (images, javascript, css...) in current directory.
 // NOTE: may need to serve files in 'resources' instead, for better security; this requires fixing urls in html files.
 app.use(express.static(__dirname));
