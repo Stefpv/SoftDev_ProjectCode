d536a8c4dfde1125e81ad13d9526728b1be4e938
mooz4305 <mooz4305@colorado.edu>
2019-11-17 17:48:39 -0700

    Installed pg-promise module. Added database functionality to server.js. Created working server-database interaction for staff-page. Replaced username with profile_pic in staffArray array.

--- HTML_Skeleton/server.js
+++ HTML_Skeleton/server.js
@@ -6,6 +6,22 @@ const bodyParser = require('body-parser');          // Add the body-parser tool 
 app.use(bodyParser.json());                         // Add support for JSON encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
 
+const pgp = require('pg-promise')();
+
+// IMPORTANT! Change these to reflect your system.
+const database_name = 'postgres';
+const database_password = 'csci';
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: database_name,
+	user: 'postgres',
+	password: database_password
+};
+
+let db = pgp(dbConfig);
+
 // statically serve the files (images, javascript, css...) in current directory.
 // NOTE: may need to serve files in 'resources' instead, for better security; this requires fixing urls in html files.
 app.use(express.static(__dirname));
@@ -34,12 +50,32 @@ app.get('/staff-page.html', function(req, res){
 });
 
 app.post('/staff-page.html/select_hall', function(req, res){
-    console.log(req.body.hall);
-    res.json({
-      staff : [
-        {username: "jack", name: "Jack", occupation: "Hall Director", description: "From Boston. Like swimming and international affairs."}
-      ]
-    });
+    var hall_name = req.body.hall;
+
+    var query = 'SELECT * FROM residentAdvisors LEFT JOIN profile_information ON residentAdvisors.student_ID = profile_information.user_ID WHERE residentAdvisors.residence_hall = \'' + hall_name + '\';';
+
+    db.any(query)
+      .then(function (rows) {
+          var staff_array = [];
+
+          for (i = 0; i < rows.length; i++) {
+            var user = rows[i];
+            staff_array.push({picture_link: user.profile_picture, name: user.user_id, occupation: user.staff_position, description: user.bio});
+          }
+
+          res.json({
+            staff : staff_array
+          });
+        })
+        .catch(function (err) {
+            // display error message
+            console.log('Could not process SQL query.', err);
+
+            // send back an empty array.
+            res.json({
+              staff : []
+            });
+        })
 });
 
 app.get('/survey.html', function(req, res){
